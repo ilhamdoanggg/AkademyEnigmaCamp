bcec3583c5fb5882da88ee333a01dfe1b32c1c94
ilhamdoanggg <ilhamdoanggg@gmail.com>
2019-09-09 01:39:54 -0700

    data dan PR

---
+++ excercise1/day1/CobaAja.java
@@ -0,0 +1,20 @@
+public class CobaAja {
+     // static int a = 1000;
+     public static void main(String[] args){
+          // System.out.println("nama");
+          // System.out.println(a);
+
+
+          int a=3;
+          int b=4;
+          int c=0;
+
+          c=b;
+          b=a;
+          a=c;
+
+          System.out.println(a);
+          System.out.println(b);
+          System.out.println(c);
+     } 
+}
