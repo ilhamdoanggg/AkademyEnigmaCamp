029d8991af8ac0df810d908445056cc7d9e9c062
ilhamdoanggg <ilhamdoanggg@gmail.com>
2019-09-17 11:18:59 +0700

    update main

--- excercise4/src/Main.java
+++ excercise4/src/Main.java
@@ -2,18 +2,17 @@ import com.enigma.model.Creep;
 import com.enigma.model.Heros;
 import com.enigma.model.Skill;
 import com.enigma.model.Tower;
-import com.sun.corba.se.impl.oa.toa.TOA;
 
 public class Main {
     public static void main(String[] args) {
         Creep grobag = new Creep(300,10);
         Skill lari = new Skill("jump", 50,100 );
-        Skill tonjok = new Skill("tonjok", 50,100 );
+        Skill tonjok = new Skill("tonjok", 50,1000 );
         Tower tower = new Tower("jump", 50,100 );
         Heros Wiro = new Heros(100, 50,"Assasin", "heros", 100, lari );
         Heros saitama = new Heros(1000, 500,"Assasin", "heros", 1000, tonjok );
 
-        saitama.castSKill(grobag);
+        saitama.castSkill(Wiro);
         System.out.println(grobag.toString());
         System.out.println(saitama.toString());
         System.out.println(Wiro.toString());
