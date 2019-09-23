444deb762419eaafea00a774f971626493be3055
ilhamdoanggg <ilhamdoanggg@gmail.com>
2019-09-24 02:37:44 +0700

    add unitTest tapi gak jadi-jadi

---
+++ excercise8/test/com/enigma/connection/DBConnectionTest.java
@@ -0,0 +1,15 @@
+package com.enigma.connection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * * Create at 9/24/2019 2:31 AM @author Ilhamdoanggg
+ **/
+public class DBConnectionTest {
+
+    @Test
+    public void letsCreateConnections() {
+    }
+}
---
+++ excercise8/test/com/enigma/service/StudentServiceTest.java
@@ -0,0 +1,23 @@
+package com.enigma.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * * Create at 9/24/2019 2:32 AM @author Ilhamdoanggg
+ **/
+public class StudentServiceTest {
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void getAllStudents() {
+    }
+
+    @Test
+    public void getSearchStudents() {
+    }
+}
