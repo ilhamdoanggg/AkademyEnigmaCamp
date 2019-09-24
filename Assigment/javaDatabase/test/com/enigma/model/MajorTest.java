fadb2621fdbd1872a35d5c6c254a77f0242b39d4
ilhamdoanggg <ilhamdoanggg@gmail.com>
2019-09-25 02:59:30 +0700

    add test abal-abal

---
+++ Assigment/javaDatabase/test/com/enigma/model/MajorTest.java
@@ -0,0 +1,31 @@
+package com.enigma.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * * Create at 9/25/2019 2:57 AM @author Ilhamdoanggg
+ **/
+public class MajorTest {
+
+    @Test
+    public void getIdMajor() {
+    }
+
+    @Test
+    public void setIdMajor() {
+    }
+
+    @Test
+    public void getManjorName() {
+    }
+
+    @Test
+    public void setManjorName() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
---
+++ Assigment/javaDatabase/test/com/enigma/service/MajorServiceTest.java
@@ -0,0 +1,19 @@
+package com.enigma.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * * Create at 9/25/2019 2:57 AM @author Ilhamdoanggg
+ **/
+public class MajorServiceTest {
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void getAllMajor() {
+    }
+}
