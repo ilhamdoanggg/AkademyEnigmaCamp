c25621da0a7d0d62a4dddd0135feaedf87ea250c
ilhamdoanggg <ilhamdoanggg@gmail.com>
2019-09-27 21:51:01 +0700

    its okay

--- excercise9/src/main/java/Main.java
+++ excercise9/src/main/java/Main.java
@@ -1,11 +1,18 @@
-import com.enigma.util.StringUtil;
+import com.enigma.config.HibernateConfigs;
+import com.enigma.model.Students;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 /**
- * * Create at 9/24/2019 4:52 PM @author Ilhamdoanggg
+ * * Create at 9/25/2019 4:08 PM @author Ilhamdoanggg
  **/
 public class Main {
     public static void main(String[] args) {
-        String hasil = StringUtil.getLeftPadding(100, 12);
 
+
+
+    SessionFactory sessionFactory = HibernateConfigs.getSessionFactory();
+    Session session = sessionFactory.getCurrentSession();
     }
 }
