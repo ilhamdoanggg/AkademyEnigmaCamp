7dac7294a2519040605c94fe725282aa28591080
ilhamdoanggg <ilhamdoanggg@gmail.com>
2019-10-09 01:29:56 +0700

    add song and integrated with artist

--- excercise14/src/main/java/com/enigma/entity/Song.java
+++ excercise14/src/main/java/com/enigma/entity/Song.java
@@ -1,15 +1,59 @@
 package com.enigma.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Song {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String nameSong;
+    private String songName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date release;
 
+    @ManyToOne
+    private Artist idArtist;
+
+    public Song() {
+    }
+
+    public Song(Integer id, String songName, Date release) {
+        this.id = id;
+        this.songName = songName;
+        this.release = release;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public Date getRelease() {
+        return release;
+    }
+
+    public void setRelease(Date release) {
+        this.release = release;
+    }
+
+    public Artist getIdArtist() {
+        return idArtist;
+    }
+
+    public void setIdArtist(Artist idArtist) {
+        this.idArtist = idArtist;
+    }
 }
