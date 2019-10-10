a99c9d69779dd675c17293fe1911ef2aac139545
ilhamdoanggg <ilhamdoanggg@gmail.com>
2019-10-10 11:09:17 +0700

    add entity

--- store-management/src/main/java/com/enigma/entity/Product.java
+++ store-management/src/main/java/com/enigma/entity/Product.java
@@ -1,4 +1,53 @@
 package com.enigma.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name ="mst_product")
 public class Product {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+    private String productName;
+    private Integer qty;
+    private BigDecimal price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+    public void deductQuantity(Integer qty){
+        this.qty= this.qty - qty;
+    }
 }
--- store-management/src/main/java/com/enigma/entity/Purchesed.java
+++ store-management/src/main/java/com/enigma/entity/Purchesed.java
@@ -1,4 +1,64 @@
 package com.enigma.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name ="trx_purchased")
 public class Purchesed {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+
+    private String productId;
+    private Integer qty;
+    private BigDecimal totalPrice;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public void setPurchesPrice(BigDecimal productPrice){
+        this.totalPrice = productPrice.multiply(new BigDecimal(this.qty));
+    }
+
+    /*public void setPurchesPrice(BigDecimal productPrice){
+        BigDecimal qtyDecimal= new BigDecimal(this.qty);
+        this.totalPrice = productPrice.multiply(qtyDecimal);
+    }*/
 }
