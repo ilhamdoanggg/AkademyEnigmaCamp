cfffdd5ae71bcf95df29ed0bf1abde4b7c4d3e6c
ilhamdoanggg <ilhamdoanggg@gmail.com>
2019-10-16 01:00:23 +0700

    add purchess views

--- Assigment/tugasUserStoreManagement/src/main/java/com/enigma/services/implement/PurchesedService.java
+++ Assigment/tugasUserStoreManagement/src/main/java/com/enigma/services/implement/PurchesedService.java
@@ -1,5 +1,6 @@
 package com.enigma.services.implement;
 
+import com.enigma.entity.Product;
 import com.enigma.entity.Purchesed;
 import com.enigma.entity.PurchesedDetail;
 import com.enigma.entity.Users;
@@ -10,6 +11,7 @@ import com.enigma.services.interfaces.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -25,11 +27,16 @@ public class PurchesedService implements PurchesedServices {
     public Purchesed purchasingDetail(Purchesed purchased) {
         Users users = userServices.getUser(purchased.getUserId());
         purchased.setUser(users);
+        BigDecimal sum = new BigDecimal(0);
         for (PurchesedDetail purchesedDetail : purchased.getPurchesedDetail()) {
+            Product product=productServices.getProductId(purchesedDetail.getProductId());
             purchesedDetail.setPurchesed(purchased);
+            purchesedDetail.setProduct(product);
             productServices.deduct(purchesedDetail.getProductId(), purchesedDetail.getQty());
             purchesedDetail.setPurchasePrice(productServices.getProductPriceById(purchesedDetail.getProductId()));
+            sum = sum.add(purchesedDetail.getSubTotalPrice());
         }
+        purchased.setTotalPrice(sum);
      /*       productServices.getProductByStoreId(purchesedDetail.getProductId());
             purchased.setTotalPricePurchased(purchesedDetail.getSubTotalPrice());*/
         return purchasedRepo.save(purchased);
@@ -37,6 +44,7 @@ public class PurchesedService implements PurchesedServices {
 
     @Override
     public List<Purchesed> getAllPurchase() {
+
         //getAllPurchesedDetail((PurchesedDetail) purchesedDetailService.getAllPurchesedDetail(new PurchesedDetail()));
         return purchasedRepo.findAll();
     }
@@ -48,7 +56,6 @@ public class PurchesedService implements PurchesedServices {
         }
         return purchasedRepo.findById(id).get();
     }
-
 }
  /*   @Override
     public List<PurchesedDetail> getAllPurchesedDetail(PurchesedDetail purchesedDetail) {
