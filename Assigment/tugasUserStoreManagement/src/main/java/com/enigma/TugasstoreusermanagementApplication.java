a6b213433de14d12992d303b18908954b68d3486
ilhamdoanggg <ilhamdoanggg@gmail.com>
2019-10-15 16:41:10 +0700

    hanya google yang tau

--- Assigment/tugasUserStoreManagement/src/main/java/com/enigma/TugasstoreusermanagementApplication.java
+++ Assigment/tugasUserStoreManagement/src/main/java/com/enigma/TugasstoreusermanagementApplication.java
@@ -9,5 +9,4 @@ public class TugasstoreusermanagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(TugasstoreusermanagementApplication.class, args);
     }
-
 }
