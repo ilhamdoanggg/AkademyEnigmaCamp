e92f7b8c29a3d5f6bc2fdcc69eaf9c9bcf55e7e3
ilhamdoanggg <ilhamdoanggg@gmail.com>
2019-10-20 03:47:22 +0700

    I 💟 Js

--- JavaScript/example/Fecth.js
+++ JavaScript/example/Fecth.js
@@ -1,6 +1,22 @@
-fetch('localhost:9090/stores')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data) // Prints result from `response.json()` in getRequest
+fetch('http://localhost:9090/users').then(respone => respone.json())
+    .then((dataUser) => {
+        console.log(dataUser);
+        fetchUserData = () => {
+            document.querySelector('.responseUser').innerHTML = JSON.stringify(dataUser)
+        }
     })
-    .catch(error => console.error(error))
+
+fetch('http://localhost:9090/storeslist').then(respone => respone.json())
+    .then((dataStore) => {
+        console.log(dataStore);
+        fetchStoreData= () => {
+            document.querySelector('.responseStore').innerHTML = JSON.stringify(dataStore)
+        }
+    })
+fetch('http://localhost:9090/products').then(respone => respone.json())
+    .then((dataProducts) => {
+        console.log(dataProducts);
+        fetchProductData = () => {
+            document.querySelector('.responseProduct').innerHTML = JSON.stringify(dataProducts)
+        }
+    })
