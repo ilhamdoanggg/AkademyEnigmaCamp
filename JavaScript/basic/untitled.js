4185742f7d6579fcb2f7335677e8c8b3d7647a39
ilhamdoanggg <ilhamdoanggg@gmail.com>
2019-11-01 04:13:40 +0700

    nyoba

---
+++ JavaScript/basic/untitled.js
@@ -0,0 +1,14 @@
+// function a(){
+//     function b(x) {
+//          function c(){
+//             console.log("ada")
+//             }
+//             c()
+//         }
+//     }
+// a()
+
+let a = [1,2,3,5,6, "angga", "Kamil", "Dedy"]
+console.log(a);
+
+a.map((element, index) => {console.log(element, index)})
